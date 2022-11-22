e2ec109bd492d06cc71b08d48880ef9a8804c312
Matheus Aguiar <matheustxaguiar@gmail.com>
2022-11-22 17:42:12 -0300

    finalizando roteador

--- index.js
+++ index.js
@@ -1,96 +1,126 @@
- /**
-  * PRimary faile for the API
-  */
- 
- var http = require('http');
- var url = require('url');
- var StringDecoder = require('string_decoder').StringDecoder;
+/*
+* Arquivo primario para a API
+* Nome: Arthur Delpupo Coelho
+* Matrícula: 20202bsi0012   
+*/
 
- // Servidor responde todas requisições com string
- var server = http.createServer(function(req, res){
+// Dependencias
 
-    // Pega a URL
-    var parseURL = url.parse(req.url, true);
+var http = require('http');
+var https = require('https')
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./config');
+var fs = require('fs');
+
+// Instanciação do servidor HTTP
+var httpServer = http.createServer(function(req,res){
+    unifiedServer(req,res);
+});
+
+// Começar o servidor na porta HTTP
+httpServer.listen(config.httpPort,function(){
+    console.log("O servidor está ouvindo na porta "+ config.httpPort);
+});
+
+// Inicialização do servidor HTTPS
+var httpsServerOptions = {
+    'key' : fs.readFileSync('./https/key.pem'),
+    'cert' : fs.readFileSync('./https/cert.pem')
+};
+
+var httpsServer = https.createServer(httpsServerOptions, function(req,res){
+    unifiedServer(req,res);
+});
+
+// Começar o servidor na porta HTTPS
+httpsServer.listen(config.httpsPort,function(){
+    console.log("O servidor está ouvindo na porta "+ config.httpsPort);
+});
 
 
-    // Pega o caminho
-    var path = parseURL.pathname;
-    var trimmePath = path.replace(/^\/+|\/+$/g, '')
 
+// 
+var unifiedServer = function(req,res){
+    
+    // Pegar a URL e analisá-la
+    var parsedUrl = url.parse(req.url,true);
+    
+    // Pegar o caminho
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
-    // Pega a query String como um objeto
-    var queryStringObject = parseURL.query;
+    // Pegar a consulta da string como um objeto
+    var queryStringObject = parsedUrl.query;
 
+    // Pegar o método HTTP
+    var method = req.method.toLowerCase();
 
-    // Coleta o HTTP Request
-    var method = req.method.toLocaleLowerCase();
-
-
-    // Pega os headers como um objeto
+    // Pegar os cabeçalhos como objetos
     var headers = req.headers;
 
-    // Coleta o payload 
-   var decoder = new StringDecoder('utf-8');
-   var buffer = '';
-   req.on('data', function(data){
-      buffer += decoder.write(data)
-   });
-   req.on('end', function(){
-      buffer += decoder.end();
+    // Pegar o payload, se nenhuma 
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
 
-      // Escolher o handlers para ir
-      var choseHandler = typeof(router[trimmePath]) !== 'undefined' ? router[trimmePath] : handlers.notFound;
+    req.on('data', function(data){
+        buffer += decoder.write(data);
+    });
 
-      //Construir o objeto
-      var data = {
-         'trimmePath': trimmePath,
-         'queryStringObject': queryStringObject,
-         'method': method,
-         'headers': headers,
-         'payload': buffer
-      };
+    req.on('end',function(data){
+        buffer += decoder.end();
 
-      choseHandler(data, function(statusCode, payload){
-         statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+        // Escolher o handler que o request irá! Ou o NotFound ou o Sample
+        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound; 
+        
+        // Construir o objeto a ser mandado para o handler 
+        var data = {
+            'trimmedPath' : trimmedPath,
+            'queryStringObject' : queryStringObject,
+            'method' : method,
+            'headers' : headers,
+            'payload' : buffer
+        };
 
-         payload = typeof(payload) == 'object' ? payload : {};
+        // Escolher a rota do request no roteador específico
+        chosenHandler(data,function(statusCode,payload){
+            //
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
 
-         var payloadString = JSON.stringify(payload);
+            //
+            payload = typeof(payload) == 'object' ? payload : {};
+            
+            // Converter o payload para string
+            var payloadString = JSON.stringify(payload);
 
-         res.writeHead(statusCode);
-         
-         // Envia o response
-         res.end(payloadString);
+            // Retornar o response
+            res.writeHead(statusCode);
+            res.end(payloadString);
 
-         // Faz o log
-         console.log('Returning this response: ', statusCode, payloadString)
+            // Registrar o caminho da request
+            console.log('Returning this response:' , statusCode, payloadString)
+        });
 
-      })
+    });
+};
 
 
-   });
 
- })
 
- // Começa o servidor e ouve na porta 3000
- server.listen(3000, function(){
-    console.log("The server is listening")
- })
-
-// Definindo handlers
+// Definir os handlers
 var handlers = {};
 
-// Sample handler
+// Handler de sample
 handlers.sample = function(data, callback){
-   callback(405, {'name':'sample handler'});
+    callback(406,{'name' : 'sample handler'});
 };
 
-// Nao encontrado
-handlers.notFound = function(data, callback){
-   callback(404);
+// Handler não encontrado
+handlers.notFound = function(data,callback){
+    callback(404);
 };
 
-// Definindo um roteador
+// Definir um roteador de request
 var router = {
-   'sample': handlers.sample
+    'sample' : handlers.sample
 };
